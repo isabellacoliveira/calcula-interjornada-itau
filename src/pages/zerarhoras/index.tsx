1066f5bf9f3024213161df07d4a82d282b36171e
Isah <isaaholiveira15@gmail.com>
2024-08-26 22:11:07 -0300

    feat: ajustes

--- src/pages/zerarhoras/index.tsx
+++ src/pages/zerarhoras/index.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import InputMask from 'react-input-mask';
 import { ButtonBack, Calcular, Container, Content, ContentButton, ContentInput, Header, LabelInput, Time, Titulo } from './styles';
 import Imagem from '../../components/imagem';
 import { useNavigate } from 'react-router-dom';
@@ -8,14 +9,21 @@ type WorkHoursCalculationResult = {
   exitTime: string;
 };
 
-const calculateExitTime = (bankHours: number, entryTime: string): WorkHoursCalculationResult => {
+const calculateExitTime = (bankHours: string, entryTime: string): WorkHoursCalculationResult => {
   const [entryHours, entryMinutes] = entryTime.split(':').map(Number);
+  const [bankHoursValue, bankMinutesValue] = bankHours.split(':').map(Number);
+  const maxWorkHoursPerDay = 10;
   const mandatoryWorkHours = 8;
   const lunchBreak = 1;
-  const totalWorkTime = mandatoryWorkHours + lunchBreak - bankHours;
+  
+  const bankHoursInHours = bankHoursValue + (bankMinutesValue / 60);
+  const totalWorkTimeRequired = mandatoryWorkHours + lunchBreak + bankHoursInHours;
+  
+  // Ensure we don't exceed the maximum work hours per day
+  const workHoursToFit = Math.min(totalWorkTimeRequired, maxWorkHoursPerDay);
 
-  let exitHours = entryHours + Math.floor(totalWorkTime);
-  let exitMinutes = entryMinutes + Math.round((totalWorkTime % 1) * 60);
+  let exitHours = entryHours + Math.floor(workHoursToFit);
+  let exitMinutes = entryMinutes + Math.round((workHoursToFit % 1) * 60);
 
   if (exitMinutes >= 60) {
     exitHours += Math.floor(exitMinutes / 60);
@@ -30,18 +38,32 @@ const calculateExitTime = (bankHours: number, entryTime: string): WorkHoursCalcu
 };
 
 export default function ZerarHoras() {
-  const [bankHours, setBankHours] = useState<number>(0);
+  const [bankHours, setBankHours] = useState<string>('00:00');
   const [entryTime, setEntryTime] = useState<string>('09:00');
   const [result, setResult] = useState<WorkHoursCalculationResult | null>(null);
   const [isVisible, setIsVisible] = useState(false);
   const navigate = useNavigate();
 
   const handleCalculate = () => {
+    if (!/^(\d{1,2}):(\d{2})$/.test(bankHours)) {
+      alert('Formato de horas no banco inválido.');
+      setResult(null);
+      return;
+    }
+
     try {
       const { exitTime } = calculateExitTime(bankHours, entryTime);
       setResult({ exitTime });
     } catch (error: any) {
       alert(error.message);
+    }
+  };
+
+  const handleChange = (setter: React.Dispatch<React.SetStateAction<string>>) => (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    const cleanedValue = value.replace(/[^0-9:]/g, '');
+    if (cleanedValue.length <= 5 && /^[0-2]?[0-9]:[0-5][0-9]?$/.test(cleanedValue)) {
+      setter(cleanedValue);
     }
   };
 
@@ -61,14 +83,16 @@ export default function ZerarHoras() {
         <Header>
           <Content>
             {!isVisible && <Imagem />}
-            <Titulo>Gestão de Horas</Titulo>
-            <ContentInput>
+            <Titulo>Em Manutenção</Titulo>
+            {/* <Titulo>Gestão de Horas</Titulo> */}
+            {/* <ContentInput>
               <LabelInput>Horas no banco:</LabelInput>
-              <Time
-                type="number"
+              <InputMask
+                mask="99:99"
                 value={bankHours}
-                onChange={(e) => setBankHours(Number(e.target.value))}
-                placeholder="Horas no banco"
+                onChange={handleChange(setBankHours)}
+                placeholder="HH:MM"
+                maskChar={null}
               />
             </ContentInput>
             <ContentInput>
@@ -84,7 +108,7 @@ export default function ZerarHoras() {
               <div>
                 <p>Hora de saída: {result.exitTime}</p>
               </div>
-            )}
+            )} */}
           </Content>
         </Header>
       </Container>
